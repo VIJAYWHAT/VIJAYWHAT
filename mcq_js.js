9f764d0af3fe9512bdfdbf62d1b2218541ac95da
VIJAYWHAT <89501520+VIJAYWHAT@users.noreply.github.com>
2022-10-11 23:27:54 +0530

    ONE WORD QUESTION MY CREATION
    With the reference of https://www.codewithrandom.com/2022/08/17/multiple-choice-quiz-code/

---
+++ mcq_js.js
@@ -0,0 +1,20 @@
+var answers = ["B", "C", "B", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
+    tot = answers.length;
+function getCheckedValue(radioName) {
+    var radios = document.getElementsByName(radioName);
+    for (var y = 0; y < radios.length; y++)
+        if (radios[y].checked) return radios[y].value;
+}
+function getScore() {
+    var score = 0;
+    for (var i = 0; i < tot; i++)
+        if (getCheckedValue("question" + i) === answers[i]) score += 1;
+    return score;
+}
+function returnScore() {
+    document.getElementById("myresults").innerHTML =
+        "Your score is " + getScore() + "/" + tot;
+    if (getScore() > 2) {
+        console.log("Bravo");
+    }
+}
